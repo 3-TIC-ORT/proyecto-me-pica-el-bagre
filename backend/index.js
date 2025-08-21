6b0cba5e0206a67eb1324b1f9dd643c216b5e80f
Thiago Dapena <you@example.com>
2025-08-21 14:24:37 -0300

    falta una banda

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+function login (contraseña, mail){
+
+}
+function register (nombre_de_usuario, sexo, mail, contraseña, escuela, nombre, apellido){
+
+}
+function comida (coordenadalugar, reseña){
+
+}
+function datosperfil (fotodeperfil, nombre_de_usuario, tusreseñashechas){
+
+}
