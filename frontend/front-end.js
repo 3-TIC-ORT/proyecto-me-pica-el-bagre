b076a6b24bb73769f04e39e330065fde3b5bb890
Elias Nahmod <you@example.com>
2025-08-29 15:49:38 -0300

    botones inicio sesion, dieño

---
+++ frontend/front-end.js
@@ -0,0 +1,12 @@
+document.getElementById
+
+
+
+function ir_a_registrar(){
+
+ window.location.href = "registrarse.html"  
+}
+
+
+
+document.getElementById("Registrarse-btn").addEventListener("click", ir_a_registrar)
