37e9b2ae47f6b8375e9b553a3c53ab49944ccc6c
Elias Nahmod <you@example.com>
2025-09-02 15:57:50 -0300

    Boton crear cuenta, no anda todavia

--- frontend/front-end.js
+++ frontend/front-end.js
@@ -5,3 +5,11 @@ boton_Registrar.addEventListener('click' , function() {
 window.location.href ='registrarse.html'
 
 });
+
+const Crear_cuenta = document.getElementById('Crearcuenta')
+
+
+Crear_cuenta.addEventListener('click' , function() { 
+window.location.href ='Iniciar-sesion.html'
+
+});
