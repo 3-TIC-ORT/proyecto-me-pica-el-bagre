78fbe90f87fae798ecc9cb2b9da652c0ef103d73
Elias Nahmod <you@example.com>
2025-09-03 15:51:38 -0300

    Diseñito registrarse (empeze nomas)

--- frontend/front-end.js
+++ frontend/front-end.js
@@ -1,7 +1,1 @@
-const boton_Registrar = document.getElementById('Registrarbtn')
-const Crear_cuenta = document.getElementById('Crearcuenta')
-
-boton_Registrar.addEventListener('click' , function() { 
-window.location.href ='registrarse.html'
-});
 
