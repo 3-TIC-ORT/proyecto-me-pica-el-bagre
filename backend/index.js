0d32b73ebb1de36d704ff80df9badada8db83265
Thiago Dapena <you@example.com>
2025-09-12 15:18:03 -0300

    po

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import fs from "fs"
+let datomail = document.getElementById("mail")
+let boton = document.getElementById("boton")
 function login (contraseña, mail){
    let datomail = document.getElementById("mail")
     fs.writeFileSync (login.json, datomail)
+    
 }
 function register (nombre_de_usuario, sexo, mail, contraseña, escuela, nombre, apellido){
 
@@ -12,4 +15,4 @@ function comida (coordenadalugar, reseña){
 function datosperfil (fotodeperfil, nombre_de_usuario, tusreseñashechas){
 
 }
-boton.addEven
+boton.addEventListener("click", login); 
