5d1d5f40e381165bb4625c979f0a984628d9198b
Thiago Dapena <you@example.com>
2025-09-19 12:10:59 -0300

    nada

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,10 @@
 import fs from "fs"
-let datomail = document.getElementById("mail")
-let boton = document.getElementById("boton")
-function login (contraseña, mail){
-   let datomail = document.getElementById("mail")
-    fs.writeFileSync (login.json, datomail)
-    
+import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+let nuevousuario =
+subscribePOSTEvent ("papu", data => {
+let contra = data.contra
+let mail = data.mail
+if(!usuarios[mail]){
+    fs.writeFileSync("login.json",mail)
 }
-function register (nombre_de_usuario, sexo, mail, contraseña, escuela, nombre, apellido){
-
-}
-function comida (coordenadalugar, reseña){
-
-}
-function datosperfil (fotodeperfil, nombre_de_usuario, tusreseñashechas){
-
-}
-boton.addEventListener("click", login); 
+})
--- frontend/front-end.js
+++ frontend/front-end.js
@@ -1,1 +1,10 @@
+import{ subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+let mail = document.getElementById("gmail-registrar")
+let contra = document.getElementById("contraseña-registrar")
+let boton = document.getElementById("boton")
+function register(){
+postEvent ("papu", contra, mail, () => {
 
+});
+}
+boton.addEventListener("click", register)
