f9883ea57cf3c00a7ec51e926f5a2d7447f67aca
Elias Nahmod <you@example.com>
2025-10-01 10:39:27 -0300

    segui con el mapa

---
+++ frontend/mapa.js
@@ -0,0 +1,10 @@
+
+
+maptilersdk.config.apiKey = '0v4Vp5ch86U3KgBsK6UG';
+
+const map = new maptilersdk.Map({
+    container: 'MapaApp',
+    style: maptilersdk.MapStyle.STREETS,
+    center: [-58.458, -34.554], 
+    zoom: 14
+});
