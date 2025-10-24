8eb01c7ed7196c57f0a28594c36b5d5540e91675
Thiago Dapena <you@example.com>
2025-10-24 12:05:51 -0300

    probamos chat

---
+++ frontend/frontcitofalso.js
@@ -0,0 +1,18 @@
+import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+import fs from "fs";
+PostEvent("papu", {nombre, apellido, sede, mail, contra}, (puchi) => {
+if (puchi === true){
+    console.log("te registraste correctamente")
+}
+else{
+    console.log ("usuario ya registrado")
+}
+});
+PostEvent("loginpapucho"), {email, contraseña}, (puchi) => {
+    if(puchi === true){
+console.log("iniciaste sesion correctamente")
+    }
+    else{
+        console.log("incorrecto")
+    }
+}
