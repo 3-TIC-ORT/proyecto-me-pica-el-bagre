ea195000379bafa25177fefb836b7c797a3be538
Elias Nahmod <you@example.com>
2025-10-24 12:12:16 -0300

    use chat para ver como deberia hcaer el mapa bn

--- frontend/mapa.js
+++ frontend/mapa.js
@@ -1,42 +1,62 @@
 let OrtBelgrano = [-34.54965800893943, -58.454123598904076];
 let OrtAlmagro = [-34.60959730083125, -58.428702612107365];
-let CentroMapa = [-34.58285, -58.443585];
+
 let Hamburguesas1 = [-34.55072465631671, -58.45312543137052]
 
 
-let map = L.map('Mapa').setView(CentroMapa, 16);
+var map = L.map('map').setView([-34.58285, -58.443585], 13); // Coordenadas iniciales
 
+// Cargar mapa desde OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+// Datos de los "botones marcadores"
+var markers = [
+  {
+    lat: -34.60959730083125,
+    lng: -58.428702612107365,
+    name: "Lugar 1",
+    description: "Descripción del lugar 1",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Red_dot.svg/1024px-Red_dot.svg.png" // Imagen del marcador
+  },
+  {
+    lat: -34.60959730083125,
+    lng: -58.428702612107365,
+    name: "Lugar 2",
+    description: "Descripción del lugar 2",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Red_dot.svg/1024px-Red_dot.svg.png"
+  }
+];
 
+// Crear un botón para cada marcador
+markers.forEach(function(marker) {
+  var button = document.createElement('button');
+  button.classList.add('custom-button');
+  button.innerHTML = `<img src="${marker.image}" alt="Marker">`;
+  
+  // Crear un marcador en el mapa usando el botón como un icono
+  var markerPosition = L.latLng(marker.lat, marker.lng);
+  
+  // Usamos el botón como icono del marcador
+  var customIcon = L.divIcon({
+    className: 'custom-icon',
+    html: button.outerHTML,
+    iconSize: [40, 40] // Tamaño del botón
+  });
 
-L.marker(OrtBelgrano)
-  .addTo(map)
-  .bindPopup("ORT Belgrano")
+  var mapMarker = L.marker(markerPosition, { icon: customIcon }).addTo(map);
 
+  // Evento de clic en el "botón marcador"
+  mapMarker.on('click', function () {
+    var infoBox = document.getElementById('info-box');
+    document.getElementById('place-name').innerText = marker.name;
+    document.getElementById('place-description').innerText = marker.description;
 
+    // Convertir la posición del marcador a píxeles para posicionar el div correctamente
+    var point = map.latLngToContainerPoint(markerPosition);
 
+    infoBox.style.left = point.x + 'px';
+    infoBox.style.top = point.y - 50 + 'px'; // Ajuste de la posición
 
-  L.marker(OrtAlmagro)
-  .addTo(map)
-  .bindPopup("Ort Almagro")
-
-
-
-let Hamburguesas1_Div = `
- <div id="Hamburguesas1" class="Hamburguesas1">
-      <main class="MainMapa"></main>
-      <footer class="FooterM">
-        <button  id="menu" class="btnmapa"> Menu</button>
-        <button  id="promociones" class="btnmapa">Promociones</button>
-        <button id="opiniones" class="btnmapa">Opiniones</button>
-      </footer>`;;
-
-
-
-
-L.marker(Hamburguesas1)
-.addTo(map)
-.bindPopup(Hamburguesas1_Div)
+    infoBox.style.display = 'block'; // Mostrar el div
+  });
+});
