3b758b1c2d8fa67b5f5baf575f05a441dedb5864
Thiago Dapena <you@example.com>
2025-10-28 14:27:50 -0300

    me quiero ir de hardware

--- backend/frontcitofalso.js
+++ backend/frontcitofalso.js
@@ -1,5 +1,13 @@
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 import fs from "fs";
+let nombre = document.getElementById("nombre-registrar");
+let apellido = document.getElementById("apellido-registrar");
+let sede = document.getElementById("Sede registro");
+let mail = document.getElementById("gmail-registrar");
+let contra = document.getElementById("contraseña-registrar");
+let email = document.getElementById("mail-login")
+let contraseña = document.getElementById("contraseña-login")
+
 PostEvent("papu", {nombre, apellido, sede, mail, contra}, (puchi) => {
 if (puchi === true){
     console.log("te registraste correctamente")
@@ -8,8 +16,8 @@ else{
     console.log ("usuario ya registrado")
 }
 });
-PostEvent("loginpapucho"), {email, contraseña}, (puchi) => {
-    if(puchi === true){
+PostEvent("loginpapucho"), {email, contraseña}, (pochi) => {
+    if(pochi === true){
 console.log("iniciaste sesion correctamente")
     }
     else{
--- frontend/front-end.js
+++ frontend/front-end.js
@@ -6,25 +6,3 @@ let sede = document.getElementById("Sede registro");
 let mail = document.getElementById("gmail-registrar");
 let contra = document.getElementById("contraseña-registrar");
 let boton = document.getElementById("boton");
-
-function register() {
-  let user = {
-    nombre: nombre.value,
-    apellido: apellido.value,
-    sede: sede.value,
-    mail: mail.value,
-    contra: contra.value,
-  };}
-
-  postEvent("papu", data, (respuesta) => {
-    if (respuesta.ok) {
-      resultado.innerText = "✅ Registro exitoso.";
-      resultado.style.color = "green";
-    } else {
-      resultado.innerText = "❌ Error al registrarse.";
-      resultado.style.color = "red";
-    }
-  });
-
-
-boton.addEventListener("click", register);
