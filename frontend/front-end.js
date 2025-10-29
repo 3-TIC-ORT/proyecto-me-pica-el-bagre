f7046a14d1b2dc396c4cde3a176362a47a4cc07c
Elias Nahmod <you@example.com>
2025-10-29 15:51:57 -0300

    empeze postevent Registrar usuario

--- frontend/front-end.js
+++ frontend/front-end.js
@@ -1,8 +1,33 @@
-import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+
 connect2Server(3000)
-let nombre = document.getElementById("nombre-registrar");
-let apellido = document.getElementById("apellido-registrar");
-let sede = document.getElementById("Sede registro");
-let mail = document.getElementById("gmail-registrar");
-let contra = document.getElementById("contraseña-registrar");
-let boton = document.getElementById("boton");
+
+let Usuario = document.getElementById("Usuario");
+let Contraseña = document.getElementById("Contraseña");
+let Genero = document.getElementById("Genero");
+
+
+
+
+function guardar() {
+
+    let generoSeleccionado = "";
+
+    if (document.getElementById("Mujer").checked) {
+        generoSeleccionado = "Mujer";
+    } else if (document.getElementById("Hombre").checked) {
+        generoSeleccionado = "Hombre";
+    } else if (document.getElementById("Otro").checked) {
+        generoSeleccionado = "Otro";
+
+    let usuarioCompleto = {
+        usuario: Usuario,
+        contraseña: Contraseña,
+        genero: Genero,
+    }
+    return(usuarioCompleto);
+    
+}}
+
+console.log(usuarioCompleto);
+
+postEvent("guardarUsuario", (usuarioCompleto) => guardar) 
