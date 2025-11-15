dc37e7a12329298d1456e2c4677cc19c6054ae98
Elias-AAA <50029956@est.ort.edu.ar>
2025-11-15 17:04:14 -0300

    HAciendo ejemplo del popup

--- frontend/mapa.js
+++ frontend/mapa.js
@@ -1,10 +1,13 @@
 let Apopup = document.getElementById('Apopup');
 let Cpopup = document.getElementById('Cpopup');
-let Popup = document.getElementById('myPopup');
-
+let Popup = document.getElementById('Popup');
+let btnInformacion = document.getElementById("btnInformacion");
+let btnMenu = document.getElementById("btnMenu");
+let btnOpiniones = document.getElementById("btnOpiniones");
 
 Apopup.addEventListener('click', () => {
-  Popup.style.display = 'flex'; 
+  Popup.style.display = 'flex';
+  MostrarS("Informacion"); 
 });
 
 
@@ -13,8 +16,23 @@ Cpopup.addEventListener('click', () => {
 });
 
 
-window.addEventListener('click', (event) => {
-  if (event.target === Popup) {
-    Popup.style.display = 'none';
-  }
+
+btnInformacion.addEventListener("click", () => {
+  MostrarS("Informacion");
 });
+
+btnMenu.addEventListener("click", () => {
+  MostrarS("Menu");
+});
+
+btnOpiniones.addEventListener("click", () => {
+  MostrarS("Opiniones");
+});
+
+function MostrarS(pipi) {
+  document.querySelectorAll(".Secciones").forEach(papuu => {
+    papuu.classList.remove("Mostrada");
+  });
+
+  document.getElementById(pipi).classList.add("Mostrada");
+}
