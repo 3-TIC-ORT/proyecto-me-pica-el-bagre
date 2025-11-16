2f712492bc826be186f30ddb2e4612ebe7e590b1
Elias-AAA <50029956@est.ort.edu.ar>
2025-11-15 22:11:52 -0300

    Casi termino el ejemplo

--- frontend/mapa.js
+++ frontend/mapa.js
@@ -3,7 +3,22 @@ let Cpopup = document.getElementById('Cpopup');
 let Popup = document.getElementById('Popup');
 let btnInformacion = document.getElementById("btnInformacion");
 let btnMenu = document.getElementById("btnMenu");
-let btnOpiniones = document.getElementById("btnOpiniones");
+let btnOpiniones = document.getElementById("btnOpiniones"); 
+let btnPromociones = document.getElementById("btnPromociones"); 
+
+let Mandar = document.getElementById("Mandar")
+let reseña = document.getElementById("Escribir")
+
+
+
+
+Escribir.addEventListener("click", () =>
+    postEvent("acuña", {Usuario:usuario.value, reseña:reseña.value}, (response) => {
+        
+    }
+));
+
+
 
 Apopup.addEventListener('click', () => {
   Popup.style.display = 'flex';
@@ -15,7 +30,9 @@ Cpopup.addEventListener('click', () => {
   Popup.style.display = 'none';
 });
 
-
+btnPromociones.addEventListener("click", () => {
+  MostrarS("Promociones");
+});
 
 btnInformacion.addEventListener("click", () => {
   MostrarS("Informacion");
